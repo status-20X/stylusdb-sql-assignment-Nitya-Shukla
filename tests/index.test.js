8e45b31d2c2b216c5feaebd98d1a57aa8f5635f1
Nitya Shukla <nityashukla444@gmail.com>
2024-03-30 19:47:19 +0530

    test case with invalid SQL query DELETE for step-3

---
+++ tests/index.test.js
@@ -0,0 +1,26 @@
+const readCSV = require('../src/csvReader');
+const parseQuery = require('../src/queryParser');
+
+test('Read CSV File', async () => {
+    const data = await readCSV('./sample.csv');
+    expect(data.length).toBeGreaterThan(0);
+    expect(data.length).toBe(3);
+    expect(data[0].name).toBe('John');
+    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+});
+
+test('Parse SQL Query', () => {
+    const query = 'SELECT id, name FROM sample';
+    const parsed = parseQuery(query);
+    expect(parsed).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
+    });
+});
+
+test('Parse Non-SELECT SQL Query', () => {
+    const query = 'DELETE FROM sample WHERE id = 1';
+    expect(() => {
+        parseQuery(query);
+    }).toThrow('Only SELECT queries are supported');
+});
