cfcb1eb244b8cdb1e13eae0f5ff882102f300564
Nitya Shukla <nityashukla444@gmail.com>
2024-03-31 15:30:06 +0530

    Step 4

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 const readCSV = require('../src/csvReader');
 const parseQuery = require('../src/queryParser');
+const executeSELECTQuery = require('../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
@@ -18,9 +19,13 @@ test('Parse SQL Query', () => {
     });
 });
 
-test('Parse Non-SELECT SQL Query', () => {
-    const query = 'DELETE FROM sample WHERE id = 1';
-    expect(() => {
-        parseQuery(query);
-    }).toThrow('Only SELECT queries are supported');
+test('Execute SQL Query', async () => {
+    const query = 'SELECT id, name FROM sample';
+    const result = await executeSELECTQuery(query);
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).not.toHaveProperty('age');
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
 });
+
