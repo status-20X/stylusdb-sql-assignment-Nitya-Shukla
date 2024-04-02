1bc16d7939046ca0058f74a0b07aefb5cf51fc49
Nitya Shukla <nityashukla444@gmail.com>
2024-04-02 10:32:35 +0530

    Step 2

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,8 @@
 const readCSV = require('../src/csvReader');
-const parseQuery = require('../src/queryParser');
-const executeSELECTQuery = require('../src/index');
+
+test('Basic Jest Test', () => {
+    expect(1).toBe(1);
+  });
 
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
@@ -10,22 +12,4 @@ test('Read CSV File', async () => {
     expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
 
-test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM sample';
-    const parsed = parseQuery(query);
-    expect(parsed).toEqual({
-        fields: ['id', 'name'],
-        table: 'sample'
-    });
-});
-
-test('Execute SQL Query', async () => {
-    const query = 'SELECT id, name FROM sample';
-    const result = await executeSELECTQuery(query);
-    expect(result.length).toBeGreaterThan(0);
-    expect(result[0]).toHaveProperty('id');
-    expect(result[0]).toHaveProperty('name');
-    expect(result[0]).not.toHaveProperty('age');
-    expect(result[0]).toEqual({ id: '1', name: 'John' });
-});
 
